8ea53c7c1861e4dfd287a4297b1f0d0912398075
wallycr <walkerpottinger@gmail.com>
2014-08-28 16:27:18 -0600

    Added Recipeml files for testing-Other updates

--- js/famchef-functions.js
+++ js/famchef-functions.js
@@ -1,6 +1,19 @@
+$(document).ready(function(){
+  var cols = document.querySelectorAll('.panel-body');
+  [].forEach.call(cols, function(col) {
+    col.addEventListener('dragstart', handleDragStart, false);
+    col.addEventListener('dragenter', handleDragEnter, false)
+    col.addEventListener('dragover',  handleDragOver, false);
+    col.addEventListener('dragleave', handleDragLeave, false);
+    col.addEventListener('drop',      handleDrop, false);
+    col.addEventListener('dragend',   handleDragEnd, false);
+  });
+});
+
 function allowDrop(ev)
 {
 	ev.preventDefault();
+  return false;
 }
 
 function drag(ev)
@@ -16,9 +29,8 @@ function drop(ev)
 }
 
 
-/*
 function handleDragStart(ev) {
-  this.style.opacity = '0.4';  // this / e.target is the source node.
+  //this.style.opacity = '0.4';  // this / e.target is the source node.
 }
 
 function handleDragOver(ev) {
@@ -27,7 +39,6 @@ function handleDragOver(ev) {
   }
 
   ev.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-
   return false;
 }
 
@@ -42,30 +53,24 @@ function handleDragLeave(ev) {
 
 function handleDrop(ev) {
   // this / e.target is current target element.
+  ev.preventDefault();
 
   if (ev.stopPropagation) {
     ev.stopPropagation(); // stops the browser from redirecting.
   }
 
   // See the section on the DataTransfer object.
+  var data = ev.dataTransfer.getData("Text");
+  ev.target.appendChild(document.getElementById(data));
 
   return false;
 }
 
 function handleDragEnd(ev) {
   // this/ev.target is the source node.
+  $(".over").removeClass("over");
 
-  [].forEach.call(cols, function (col) {
+  /*[].forEach.call(cols, function (col) {
     col.classList.remove('over');
-  });
+  });*/
 }
-
-var cols = document.querySelectorAll('.panel-body');
-[].forEach.call(cols, function(col) {
-  col.addEventListener('dragstart', handleDragStart, false);
-  col.addEventListener('dragenter', handleDragEnter, false)
-  col.addEventListener('dragover', handleDragOver, false);
-  col.addEventListener('dragleave', handleDragLeave, false);
-  col.addEventListener('drop', handleDrop, false);
-  col.addEventListener('dragend', handleDragEnd, false);
-});*/
